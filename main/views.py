25d857340463f28dc122beb695523aedfde2fb28
Shermukhammad Kayumov <shermuhammad.qayumov.02@mail.ru>
2023-07-10 04:42:04 -0700

    all templates created demo database is beingWaited

--- main/views.py
+++ main/views.py
@@ -2,6 +2,23 @@ from django.shortcuts import render
 from .models import KpiModel
 
 # Create your views here.
+
 def index(request):
     kpi_models = KpiModel.objects.all()
     return render(request, 'index.html', {"kpi":kpi_models})
+
+
+def book(request):
+    return render(request, 'book.html')
+
+
+def sport(request):
+    return render(request, 'sport.html')
+
+
+def work(request):
+    return render(request, 'work.html')
+
+
+def eureka(request):
+    return render(request, 'eureka.html')
