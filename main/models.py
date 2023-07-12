b45e33bee654756560d775722a443f794ece7e9d
SamWiz13 <samwith75@gmail.com>
2023-07-12 10:16:00 +0500

    description field

--- main/models.py
+++ main/models.py
@@ -11,16 +11,14 @@ class WorkManager(models.Manager):
 class WorkModel(models.Model):
     deadline = models.DateField()
     score = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField(max_length=200, null=True, blank=True)
     kpi = models.ForeignKey("KpiModel", on_delete=models.CASCADE, related_name="work_items")
 
     objects = WorkManager()
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         self.kpi.calculate_general()
-
-
-    def __str__(self):
-        return str(self.score) + " " + str(self.deadline)
+    return str(self.score) + " " + str(self.deadline)
     
 
 
